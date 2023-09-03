c354469cf25ed6ae98ae4e9111e9294c7fd739fb
candelaojeda <ojedacandelaco@gmail.com>
2023-09-03 15:43:38 -0300

    added getters

--- src/store/index.js
+++ src/store/index.js
@@ -4,8 +4,39 @@ import TransactionsServices from "./../services/TransactionsServices.js";
 export default createStore({
   state: {
     idUser: "",
+    totalMoney: null,
+    transactions: [],
   },
-  getters: {},
+  getters: {
+    getTransactions: (state) => {
+      return state.transactions;
+    },
+    getCurrentStatus: (state) => {
+      const wallet = [];
+      state.transactions.forEach((transaction) => {
+        const index = wallet.findIndex((element) => element.crypto_code == transaction.crypto_code);
+        if (index == -1) {
+          if (transaction.action == "sale") {
+            const negativeTransaction = transaction;
+            negativeTransaction.crypto_amount = -negativeTransaction.crypto_amount;
+            negativeTransaction.money = -negativeTransaction.money;
+            wallet.push(negativeTransaction);
+          } else {
+            wallet.push(transaction);
+          }
+        } else {
+          if (transaction.action == "purchase") {
+            wallet[index].crypto_amount += transaction.crypto_amount;
+            wallet[index].money += transaction.money;
+          } else {
+            wallet[index].crypto_amount -= transaction.crypto_amount;
+            wallet[index].money -= transaction.money;
+          }
+        }
+      });
+      return wallet;
+    },
+  },
   mutations: {
     modifyIdUser(state, idUser) {
       state.idUser = idUser;
@@ -18,6 +49,9 @@ export default createStore({
         .catch(() => {
           alert("Error");
         });
+    },
+    modifyTotalMoneyActualState(state, totalMoney) {
+      state.totalMoney = totalMoney;
     },
   },
   actions: {},
