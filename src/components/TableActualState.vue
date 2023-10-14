a529ae5f0a69eb25b8441384dfe9024d425f0fb9
candelaojeda <ojedacandelaco@gmail.com>
2023-10-14 16:58:15 -0300

    modified handle variable money

--- src/components/TableActualState.vue
+++ src/components/TableActualState.vue
@@ -9,16 +9,10 @@
         </tr>
       </thead>
       <tbody>
-        <tr>
-          <td class="tableData">
-            <p v-for="coin in wallet" :key="coin.crypto_code">{{ coin.crypto_code }}</p>
-          </td>
-          <td class="tableData">
-            <p v-for="coin in wallet" :key="coin.crypto_code">{{ coin.crypto_amount }}</p>
-          </td>
-          <td class="tableData">
-            <p v-for="coin in wallet" :key="coin.crypto_code">${{ coin.money }}</p>
-          </td>
+        <tr v-for="(coin, index) in wallet" :key="index">
+          <td class="tableData">{{ coin.crypto_code }}</td>
+          <td class="tableData">{{ coin.crypto_amount }}</td>
+          <td class="tableData">${{ calculateMoney(coin) }}</td>
         </tr>
         <tr>
           <td class="tableTitle">
@@ -31,8 +25,12 @@
         </tr>
       </tbody>
     </table>
+    <button v-on:click="history()" type="submit" class="btnHistory">
+      <b>Check history</b>
+    </button>
   </div>
 </template>
+
 <script>
 import { mapGetters } from "vuex";
 
@@ -43,11 +41,39 @@ export default {
     }),
   },
   methods: {
+    history: function () {
+      this.$router.push("/history");
+    },
+    calculateMoney(coin) {
+      const cryptoAmount = parseFloat(coin.crypto_amount);
+      const money = parseFloat(coin.money);
+      return (cryptoAmount * money).toFixed(2);
+    },
     calculateTotal() {
-      let total = 0;
+      let cryptoTotals = {};
+
       for (const coin of this.wallet) {
-        total += parseFloat(coin.money);
+        const cryptoCode = coin.crypto_code;
+        const cryptoAmount = parseFloat(coin.crypto_amount);
+        const money = parseFloat(coin.money);
+
+        if (cryptoCode in cryptoTotals) {
+          cryptoTotals[cryptoCode].quantity += cryptoAmount;
+          cryptoTotals[cryptoCode].totalMoney += money;
+        } else {
+          cryptoTotals[cryptoCode] = {
+            quantity: cryptoAmount,
+            totalMoney: money,
+          };
+        }
       }
+
+      let total = 0;
+
+      for (const cryptoCode in cryptoTotals) {
+        total += cryptoTotals[cryptoCode].totalMoney;
+      }
+
       return total.toFixed(2);
     },
   },
@@ -96,5 +122,26 @@ export default {
 }
 .tableData {
   font-weight: bold;
+}
+.btn:hover,
+.btnHistory:hover {
+  background: azure;
+  color: #adb942;
+  border-radius: 5px;
+}
+.btnHistory {
+  width: 200px;
+  height: 48px;
+  background: #adb942;
+  font-size: 20px;
+  text-transform: uppercase;
+  letter-spacing: 5px;
+  color: azure;
+  letter-spacing: 1px;
+  border-radius: 5px;
+  cursor: pointer;
+  margin: 40px auto;
+  border-style: none;
+  transition: background 0.3s ease-in-out;
 }
 </style>
